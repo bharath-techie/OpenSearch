a08ac91f9ec70433c7ea3e7ed8daa97967942303
Bharathwaj G <bharath78910@gmail.com>
2023-10-22 14:12:39 +0530

    Fixes for tests
    Signed-off-by: Bharathwaj G <bharath78910@gmail.com>

--- test/framework/src/main/java/org/opensearch/test/InternalTestCluster.java
+++ test/framework/src/main/java/org/opensearch/test/InternalTestCluster.java
@@ -2723,6 +2723,7 @@ public final class InternalTestCluster extends TestCluster {
                     false,
                     false,
                     false,
+                    false,
                     false
                 );
                 assertThat(
