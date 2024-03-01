1d44e1a7b2d0fe1b98f648eefc5e70ff2de959c2
Bharathwaj G <bharath78910@gmail.com>
2024-03-01 16:13:27 +0530

    Pre compute changes
    Signed-off-by: Bharathwaj G <bharath78910@gmail.com>

--- server/src/main/java/org/opensearch/index/codec/PerFieldMappingPostingFormatCodec.java
+++ server/src/main/java/org/opensearch/index/codec/PerFieldMappingPostingFormatCodec.java
@@ -36,9 +36,10 @@ import org.apache.logging.log4j.Logger;
 import org.apache.lucene.codecs.Codec;
 import org.apache.lucene.codecs.DocValuesFormat;
 import org.apache.lucene.codecs.PostingsFormat;
-import org.apache.lucene.codecs.lucene90.Lucene90DocValuesFormat;
 import org.apache.lucene.codecs.lucene95.Lucene95Codec;
 import org.opensearch.common.lucene.Lucene;
+import org.opensearch.index.codec.startree.codec.StarTreeCodec;
+import org.opensearch.index.codec.startree.codec.StarTreeDocValuesFormat;
 import org.opensearch.index.mapper.CompletionFieldMapper;
 import org.opensearch.index.mapper.MappedFieldType;
 import org.opensearch.index.mapper.MapperService;
@@ -53,18 +54,18 @@ import org.opensearch.index.mapper.MapperService;
  *
  * @opensearch.internal
  */
-public class PerFieldMappingPostingFormatCodec extends Lucene95Codec {
+public class PerFieldMappingPostingFormatCodec extends StarTreeCodec { // TODO : this is a hack , can't extend startreecodec
     private final Logger logger;
     private final MapperService mapperService;
-    private final DocValuesFormat dvFormat = new Lucene90DocValuesFormat();
+    private final DocValuesFormat dvFormat = new StarTreeDocValuesFormat();
 
     static {
         assert Codec.forName(Lucene.LATEST_CODEC).getClass().isAssignableFrom(PerFieldMappingPostingFormatCodec.class)
             : "PerFieldMappingPostingFormatCodec must subclass the latest " + "lucene codec: " + Lucene.LATEST_CODEC;
     }
 
-    public PerFieldMappingPostingFormatCodec(Mode compressionMode, MapperService mapperService, Logger logger) {
-        super(compressionMode);
+    public PerFieldMappingPostingFormatCodec(Lucene95Codec.Mode compressionMode, MapperService mapperService, Logger logger) {
+        super();
         this.mapperService = mapperService;
         this.logger = logger;
     }
@@ -82,6 +83,11 @@ public class PerFieldMappingPostingFormatCodec extends Lucene95Codec {
 
     @Override
     public DocValuesFormat getDocValuesFormatForField(String field) {
+        return dvFormat;
+    }
+
+    @Override
+    public final DocValuesFormat docValuesFormat() {
         return dvFormat;
     }
 }
