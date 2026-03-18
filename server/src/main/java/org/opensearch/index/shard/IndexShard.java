f74df9f65c725a3f716e3a2ac71a25065ec7e5e7
Bukhtawar Khan <bukhtawa@amazon.com>
2026-03-18 18:52:44 +0530

    Refactor CompositeEngine to use factory
    Signed-off-by: Bukhtawar Khan <bukhtawa@amazon.com>

--- server/src/main/java/org/opensearch/index/shard/IndexShard.java
+++ server/src/main/java/org/opensearch/index/shard/IndexShard.java
@@ -145,7 +145,7 @@ import org.opensearch.index.engine.RefreshFailedEngineException;
 import org.opensearch.index.engine.SafeCommitInfo;
 import org.opensearch.index.engine.Segment;
 import org.opensearch.index.engine.SegmentsStats;
-import org.opensearch.index.engine.exec.DataFormatRegistry;
+import org.opensearch.index.engine.exec.CompositeEngineFactory;
 import org.opensearch.index.fielddata.FieldDataStats;
 import org.opensearch.index.fielddata.ShardFieldData;
 import org.opensearch.index.flush.FlushStats;
@@ -406,7 +406,7 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
     // Used to limit the number of concurrent translog tasks. When the semaphore is exhausted, serial recovery is used.
     private static final Semaphore translogConcurrentRecoverySemaphore = new Semaphore(1000);
 
-    private final DataFormatRegistry dataFormatRegistry;
+    private final CompositeEngineFactory compositeEngineFactory;
 
     @InternalApi
     public IndexShard(
@@ -448,7 +448,7 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
         final ClusterApplierService clusterApplierService,
         @Nullable final MergedSegmentPublisher mergedSegmentPublisher,
         @Nullable final ReferencedSegmentsPublisher referencedSegmentsPublisher,
-        @Nullable final DataFormatRegistry dataFormatRegistry
+        @Nullable final CompositeEngineFactory compositeEngineFactory
     ) throws IOException {
         super(shardRouting.shardId(), indexSettings);
         assert shardRouting.initializing();
@@ -574,9 +574,9 @@ public class IndexShard extends AbstractIndexShardComponent implements IndicesCl
                 startRefreshTask();
             }
         }
-        this.dataFormatRegistry = dataFormatRegistry;
-        if (dataFormatRegistry != null) {
-            this.currentCompositeEngineReference.set(new CompositeEngine(dataFormatRegistry));
+        this.compositeEngineFactory = compositeEngineFactory;
+        if (compositeEngineFactory != null) {
+            this.currentCompositeEngineReference.set(compositeEngineFactory.create());
         }
     }
 
