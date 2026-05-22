df8e084efa23e7b62a39f6a28441e40f236b2822
G <bharath78910@gmail.com>
2026-05-22 18:34:56 +0530

    integrating indices cache with analytics engine
    Signed-off-by: G <bharath78910@gmail.com>

--- sandbox/plugins/analytics-backend-lucene/src/main/java/org/opensearch/be/lucene/LuceneAnalyticsBackendPlugin.java
+++ sandbox/plugins/analytics-backend-lucene/src/main/java/org/opensearch/be/lucene/LuceneAnalyticsBackendPlugin.java
@@ -8,6 +8,7 @@
 
 package org.opensearch.be.lucene;
 
+import org.apache.calcite.sql.SqlKind;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.lucene.search.IndexSearcher;
@@ -30,7 +31,6 @@ import org.opensearch.tasks.CancellableTask;
 import org.opensearch.tasks.Task;
 
 import java.util.HashSet;
-import org.apache.calcite.sql.SqlKind;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -167,6 +167,12 @@ public class LuceneAnalyticsBackendPlugin implements AnalyticsSearchBackendPlugi
         IndexReaderProvider.Reader reader = shardCtx.getReader();
         LuceneReader luceneReader = reader.getReader(plugin.getDataFormat(), LuceneReader.class);
         IndexSearcher searcher = new IndexSearcher(luceneReader.directoryReader());
+        if (shardCtx.getQueryCache() != null) {
+            searcher.setQueryCache(shardCtx.getQueryCache());
+        }
+        if (shardCtx.getQueryCachingPolicy() != null) {
+            searcher.setQueryCachingPolicy(shardCtx.getQueryCachingPolicy());
+        }
         QueryShardContext queryShardContext = buildMinimalQueryShardContext(shardCtx, searcher);
         BooleanSupplier isCancelled = () -> {
             Task task = shardCtx.getTask();
